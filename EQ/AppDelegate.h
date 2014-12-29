9c85c7b47173870a50905c6313c3ce2b138900f3
YangYuxin <yangyuxin@YangYuxindeMacBook-Pro.local>
2014-12-29 21:39:52 +0800

    Initial Commit

---
+++ EQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EQ
+//
+//  Created by YangYuxin on 14/12/29.
+//  Copyright (c) 2014年 YYX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EQ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EQ
+//
+//  Created by YangYuxin on 14/12/29.
+//  Copyright (c) 2014年 YYX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
