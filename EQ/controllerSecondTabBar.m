1cb2703366c1fdc8f1307ba02640768784f58dd4
YangYuxin <yangyuxin@YangYuxindeMacBook-Pro.local>
2015-01-05 00:04:07 +0800

    pageControl

---
+++ EQ/controllerSecondTabBar.m
@@ -0,0 +1,37 @@
+//
+//  controllerSecondTabBar.m
+//  EQ
+//
+//  Created by YangYuxin on 15/1/4.
+//  Copyright (c) 2015年 YYX. All rights reserved.
+//
+
+#import "controllerSecondTabBar.h"
+
+@interface controllerSecondTabBar ()
+
+@end
+
+@implementation controllerSecondTabBar
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
